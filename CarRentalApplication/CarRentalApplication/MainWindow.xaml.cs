d52a901fb2aab3f3337438b5f508b6f7dd0ddcf4
PsychoHacks <PsychoHacks@users.noreply.github.com>
2017-04-29 22:36:31 +0100

    + More documentation to explaint the program (Needs more in the searhc query)

--- CarRentalApplication/CarRentalApplication/MainWindow.xaml.cs
+++ CarRentalApplication/CarRentalApplication/MainWindow.xaml.cs
@@ -33,56 +33,66 @@ namespace CarRentalApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+        //All variables that are used across this file are placed here
         CarRental_S00162685Entities db = new CarRental_S00162685Entities();
-        public enum CarSizes { All, Small, Medium, Large };
+        public enum CarSizes { All, Small, Medium, Large }; //Used for the combo box at the top of the window
 
         DateTime startDate, endDate;
         string startDateString, endDateString, imageDirectory;
-
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
+        #region Loading The Program
         private void SetImageDirectory()
         {
-            //Setting the directory for the images
+            //Setting the directory for the images outside of the "bin" folder
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo parent = Directory.GetParent(currentDirectory);
             DirectoryInfo grandparent = parent.Parent;
             currentDirectory = grandparent.FullName;
             imageDirectory = currentDirectory + "\\images";
         }
-
+        //When the window for the program is loaded, this method is called
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             SetImageDirectory();
-
+            //Enum list gets set to combo box
             cbxCarType.ItemsSource = Enum.GetNames(typeof(CarSizes));
             cbxCarType.SelectedIndex = 0;
-
+            //Sets the default dates to today and tomrrow
             dpStartDate.SelectedDate = DateTime.Now;
             dpEndDate.SelectedDate = DateTime.Now.AddDays(1);
-
+            //Default image used by the program
             imgCar.Source = new BitmapImage(new Uri(imageDirectory + "\\logo.png", UriKind.Absolute));
         }
+        #endregion
+        #region Before The Search
+                //I realise this is duplicated code, but I don't know of a neater solution
+                private void dpStartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+                {
+                    //When the dates are changed, the boxes are cleared so the user doesn't input the wrong information
+                    lbxAvailableCars.ItemsSource = null;
+                    tblkSelectedCar.Text = "";
+                    imgCar.Source = new BitmapImage(new Uri(imageDirectory + "\\logo.png", UriKind.Absolute));
+                }
 
-        private void dpEndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        {
-            lbxAvailableCars.ItemsSource = null;
-            tblkSelectedCar.Text = "";
-        }
-
-        private void dpStartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        {
-            lbxAvailableCars.ItemsSource = null;
-            tblkSelectedCar.Text = "";
-        }
+                private void dpEndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+                {
+                    lbxAvailableCars.ItemsSource = null;
+                    tblkSelectedCar.Text = "";
+                    imgCar.Source = new BitmapImage(new Uri(imageDirectory + "\\logo.png", UriKind.Absolute));
+                }
+        #endregion
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            //Gets the selected size
             string selectedSize = cbxCarType.SelectedValue.ToString();
-
+            //Runs different query on the selection
             if (selectedSize == "All")
             {
                 var searchQuery = from b in db.Bookings
@@ -94,7 +104,7 @@ namespace CarRentalApplication
                 var searchQuery2 = from c in db.Cars
                                    where !searchQuery.Contains(c.CarID)
                                    select c;
-
+                //Sets the query results to the listbox
                 lbxAvailableCars.ItemsSource = searchQuery2.ToList();
             }
 
@@ -118,18 +128,20 @@ namespace CarRentalApplication
 
         private void lbxAvailableCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //Gets the currently selected car
             Car selectedCar = lbxAvailableCars.SelectedValue as Car;
 
             if (selectedCar != null)
             {
+                //Gets the selected startdate and enddate to be stored into the database
                 startDate = dpStartDate.SelectedDate.Value;
                 startDateString = startDate.ToString("dd/MM/yyyy");
 
                 endDate = dpEndDate.SelectedDate.Value;
                 endDateString = endDate.ToString("dd/MM/yyyy");
-
+                //Calls the GetCarDetails method in the Car class
                 tblkSelectedCar.Text = selectedCar.GetCarDetails(startDateString, endDateString);
-
+                //Changes the image displayed depending on the car selected, depending on the size
                 switch (selectedCar.CarID)
                 {
                     case 4:
@@ -169,10 +181,11 @@ namespace CarRentalApplication
 
             if (selectedCar != null)
             {
+                //Calls the GetCarDetails once again for the message box
                 messageBoxString = selectedCar.GetCarDetails(startDateString, endDateString);
 
                 MessageBox.Show("Booking Confirmation\n\n" + messageBoxString);
-
+                //Inserts the data into the database and saves changes
                 Booking b = new Booking()
                 {
                     StartDate = startDate,
@@ -182,10 +195,10 @@ namespace CarRentalApplication
 
                 db.Bookings.Add(b);
                 db.SaveChanges();
-
+                //Reloads the program to the start, clearing all boxes
                 Window_Loaded(sender, e);
             }
-
+            //A car must be selected before the button will activate
             else
             {
                 MessageBox.Show("Please select a car!");
--- CarRentalApplication/CarRentalApplication/Partial Classes/Car.cs
+++ CarRentalApplication/CarRentalApplication/Partial Classes/Car.cs
@@ -7,11 +7,13 @@
     {
         public override string ToString()
         {
+            //Display for the listbox
             return Make + " - " + Model;
         }
 
         public string GetCarDetails(string start, string end)
         {
+            //Displayed on tblkSelectedCar and MessageBox for btnBook
             return "Car ID: " + CarID + "\nMake: " + Make + "\nModel: " + Model
                 + "\nRental Date: " + start + "\nReturn Date: " + end;
         }
